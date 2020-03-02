f9f170bdb97952eca0eee225c1d5c970152a2efc
Joaquin Vargas <joaquin.vargas90@gmail.com>
2020-03-01 21:09:17 -0600

    Update index.js

--- solo-project/src/index.js
+++ solo-project/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
