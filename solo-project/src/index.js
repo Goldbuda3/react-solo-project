04f8d1c2c04e2c7e584939ab6375daf790bc87b1
Joaquin Vargas <joaquin.vargas90@gmail.com>
2020-03-02 22:15:44 -0600

    tests

--- solo-project/src/index.js
+++ solo-project/src/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// // import { Provider } from 'react-redux'
+// // import store from "./redux/store";
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './index.css';
+
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import reducer from "./reducer";
+
+import LoginForm from "./LoginForm";
+
+const store = createStore(reducer);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Card body>
+          <LoginForm />
+        </Card>
+      </div>
+    </Provider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
